7b73025d7f50218206215845d7192b11fbe15ca3
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-08-08 15:59:14 -0600

    nav bar 3 fix

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import FavIcon from './FavIcon';
-
 import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={!isFavPhotoExist}/>
+      {/* Ensure alert is shown when a favorite photo exists */}
+      <FavIcon displayAlert={isFavPhotoExist} />
     </div>
-  ) 
+  ); 
 };
 
 export default FavBadge;
