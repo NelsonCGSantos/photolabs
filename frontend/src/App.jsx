7d03f9387af9f28d4abafaf588c1ef8a4d449e5a
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-08-08 15:31:01 -0600

    photo topics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 //import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
 
 
 
 const App = () => (
   <div className="App">
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-    <PhotoList/>
+    <TopicList />
+    <PhotoList />
   </div>
-)
+);
 
 export default App;
