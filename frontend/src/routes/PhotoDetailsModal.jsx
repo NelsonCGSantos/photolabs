b3c097747940fb336a564f9306c605b5cd8e98aa
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-08-10 12:44:05 -0600

    add photo details

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,6 +3,8 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
+  console.log('Selected Photo:', props.photo);
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={props.onClose}>
